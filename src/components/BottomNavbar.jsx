20830eaeb1f81e44b70880218942f5abb5e0d00b
mina-corrado <mina.corrado@hotmail.it>
2023-04-29 23:12:45 +0200

    nome sito, link

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -1,6 +1,7 @@
 import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
 import { UserContext } from '../context/Context';
+import { Link } from 'react-router-dom';
 
 const BottomNavbar = () => {
     const {user} = useContext(UserContext);
@@ -8,23 +9,23 @@ const BottomNavbar = () => {
     <>
         <div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
             <Nav defaultActiveKey="/" className="flex-column">
-                <Nav.Link href="/catalogo">Catalogo</Nav.Link>
-                <Nav.Link href="/offerte">Offerte</Nav.Link>
+                <Nav.Link as={Link} to="/catalogo">Catalogo</Nav.Link>
+                <Nav.Link as={Link} to="/offerte">Offerte</Nav.Link>
             </Nav>
             <Nav className="flex-column">
-                <Nav.Link href="/ordine">Ordine</Nav.Link>
+                <Nav.Link as={Link} to="/ordine">Ordine</Nav.Link>
                 {!user &&
-                    <Nav.Link eventKey="link-1" href='/login'>Accedi</Nav.Link>
+                    <Nav.Link eventKey="link-1" as={Link} to='/login'>Accedi</Nav.Link>
                 }
                 {
                     user && user.isAdmin &&
-                    <Nav.Link eventKey="link-2" href='/admin'>Admin</Nav.Link>
+                    <Nav.Link eventKey="link-2" as={Link} to='/admin'>Admin</Nav.Link>
                 }
             </Nav>
         </div>
         <Nav className="flex-column">
             <Nav.Link className="text-center" eventKey="disabled" disabled>
-                Mina's Rose Garden - &copy;2023
+                Mina Rose Garden - &copy;2023
             </Nav.Link>
         </Nav>
     </>
